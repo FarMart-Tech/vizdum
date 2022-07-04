4030f7db41a8ea4bcae5c6ad625e39e397d599c2
Deepak Mishra <trtl@Deepaks-MacBook-Pro.local>
2022-07-04 14:41:55 +0530

    added util functions

--- dashboard/nav_2/side_1/data.py
+++ dashboard/nav_2/side_1/data.py
@@ -6,9 +6,10 @@ import pandas as pd
 import time
 import numpy as np
 import plotly as px
-#from extras.db import datasets
-#from extras.utils import lru_ttl_cache
+from extras.utils import lru_ttl_cache
 
+
+@lru_ttl_cache(ttl_seconds=60)
 def side_1_db():
     df = px.data.gapminder().query("year==2007")
     return df
